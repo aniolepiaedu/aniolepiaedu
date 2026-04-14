a665f4119c45e34caeea3d3e515fcfbb02109f55
Aniol Rodríguez <aniolrodriguez2005@gmail.com>
2026-04-14 17:48:05 +0200

    frontend2

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+// 🔥 servir carpeta public
+app.use(express.static(path.join(__dirname, "public")));
 
-// importar rutas
+// rutas API
 const favoritesRoutes = require("./api/favorites");
 const historyRoutes = require("./api/history");
 const wishlistRoutes = require("./api/wishlist");
 
-// usar rutas
 app.use("/api/favorites", favoritesRoutes);
 app.use("/api/history", historyRoutes);
 app.use("/api/wishlist", wishlistRoutes);
 
-// ruta base
+// ruta base → index.html
 app.get("/", (req, res) => {
-    res.send("API is running 🚀");
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
